05403b1ac7ac88cda56377f8420dfc7d1f83db37
Niall Devlin <niallcdevlin@gmail.com>
2021-02-23 13:03:47 -0700

    fixing errors

--- ros/src/waypoint_updater/waypoint_updater.py
+++ ros/src/waypoint_updater/waypoint_updater.py
@@ -48,7 +48,7 @@ class WaypointUpdater(object):
     def loop(self):
         rate = rospy.Rate(50)
         while not rospy.is_shutdown():
-            if self.pose and self.base_waypoints:
+            if self.pose and self.waypoint_tree:
                 self.publish_waypoints(self.get_closest_waypoint_idx())
             rate.sleep()
 
